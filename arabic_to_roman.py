45b3305951dcc98442b806a77cfc73f58e536226
Danny Trinh <djtrinh@bu.edu>
2020-01-22 19:09:11 -0800

    Updated requirements and added more complex test

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -15,7 +15,7 @@ def arabic_to_roman(dec_input):
     for pair in arabic_to_roman_pairs:
         count = dec_input//pair[0]  # how many multiples of each pair
         dec_input -= count * pair[0]  # subtract total pair value from input
-        roman_output += (count * pair[1]) # concatenate number of roman numerals
+        roman_output += (count * pair[1])  # concatenate number of roman numerals
     return roman_output
 
 
@@ -23,21 +23,26 @@ def test_arabic_to_roman_float():
     with pytest.raises(TypeError):
         arabic_to_roman(3.14)
 
-       
+
 def test_arabic_to_roman_string():
     with pytest.raises(TypeError):
         arabic_to_roman("oinkies")
 
-       
+
 def test_arabic_to_roman_low_bound():
     with pytest.raises(ValueError):
         arabic_to_roman(0)
 
-       
+
 def test_arabic_to_roman_upper_bound():
     with pytest.raises(ValueError):
         arabic_to_roman(9000)
 
-       
-def test_arabic_to_roman_simple():
-    assert arabic_to_roman(1456) == "MCDLVI"
+
+test_cases = [(1, "I"), (4, "IV"), (9, "IX"), (29, "XXIX"), (143, "CXLIII"),
+              (1456, "MCDLVI"), (2775, "MMDCCLXXV"), (3714, "MMMDCCXIV")]
+
+
+def test_arabic_to_roman():
+    for num, rom in test_cases:
+        assert arabic_to_roman(num) == rom
