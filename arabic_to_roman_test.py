6ffedf514f988b6c0f9beee206ae9b810aebee86
Danny Trinh <djtrinh@bu.edu>
2020-02-19 15:59:47 -0800

    Updated test to pass method correctly

--- arabic_to_roman_test.py
+++ arabic_to_roman_test.py
@@ -1,24 +1,25 @@
 import pytest
+import arabic_to_roman as a2r
 
 
 def test_arabic_to_roman_float():
     with pytest.raises(TypeError):
-        arabic_to_roman(3.14)
+        a2r.arabic_to_roman(3.14)
 
 
 def test_arabic_to_roman_string():
     with pytest.raises(TypeError):
-        arabic_to_roman("oinkies")
+        a2r.arabic_to_roman("oinkies")
 
 
 def test_arabic_to_roman_low_bound():
     with pytest.raises(ValueError):
-        arabic_to_roman(0)
+        a2r.arabic_to_roman(0)
 
 
 def test_arabic_to_roman_upper_bound():
     with pytest.raises(ValueError):
-        arabic_to_roman(9000)
+        a2r.arabic_to_roman(9000)
 
 
 test_cases = [(1, "I"), (4, "IV"), (9, "IX"), (29, "XXIX"), (143, "CXLIII"),
@@ -27,4 +28,4 @@ test_cases = [(1, "I"), (4, "IV"), (9, "IX"), (29, "XXIX"), (143, "CXLIII"),
 
 def test_arabic_to_roman():
     for num, rom in test_cases:
-        assert arabic_to_roman(num) == rom
+        assert a2r.arabic_to_roman(num) == rom
